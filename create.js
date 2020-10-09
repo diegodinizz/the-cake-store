109cf6b8baf754b0ef3d56db4aeb3c3e47c70505
Diego Diniz <diegodinizz@gmail.com>
2020-10-09 17:03:18 +0100

    added missing semicolon

--- create.js
+++ create.js
@@ -1,10 +1,10 @@
-import * as uuid from 'uuid'
-import handler from './libs/handler-lib'
-import dynamoDb from './libs/dynamodb-lib'
+import * as uuid from 'uuid';
+import handler from './libs/handler-lib';
+import dynamoDb from './libs/dynamodb-lib';
 
 export const main = handler(async (event, context) => {
   // Request body is passed in as a JSON encoded string in 'event.body'
-  const data = JSON.parse(event.body)
+  const data = JSON.parse(event.body);
   const params = {
     TableName: process.env.tableName,
     // 'Item' contains the attributes of the item to be created
@@ -20,9 +20,9 @@ export const main = handler(async (event, context) => {
       imageUrl: data.imageUrl,
       yumFactor: data.yumFactor
     }
-  }
+  };
 
-  await dynamoDb.put(params)
+  await dynamoDb.put(params);
 
-  return params.Item
-})
+  return params.Item;
+});
